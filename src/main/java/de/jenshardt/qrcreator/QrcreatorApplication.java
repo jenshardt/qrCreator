13b1ca6067ea45024060915354f973f8b52c6920
Jens Hardt <jenshardt@gmx.net>
2025-12-29 09:37:57 +0100

    init

---
+++ src/main/java/de/jenshardt/qrcreator/QrcreatorApplication.java
@@ -0,0 +1,13 @@
+package de.jenshardt.qrcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcreatorApplication.class, args);
+	}
+
+}
